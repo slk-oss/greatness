24709e1cb2b205420bb159df251c2f20f1a099ab
slk <slk.mstu@mail.ru>
2026-03-31 01:14:27 +0300

    Restore Xcode project

---
+++ greatness/greatness/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  greatness
+//
+//  Created by Сулейман Курбанов on 31.03.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ greatness/greatness/greatnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  greatnessApp.swift
+//  greatness
+//
+//  Created by Сулейман Курбанов on 31.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct greatnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
