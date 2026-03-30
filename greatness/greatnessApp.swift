fbbd6c96f801c75a6abc238b1937362c3ca04e4c
slk <slk.mstu@mail.ru>
2026-03-30 23:01:15 +0300

    Initial Commit

---
+++ greatness/greatnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  greatnessApp.swift
+//  greatness
+//
+//  Created by Сулейман Курбанов on 30.03.2026.
+//
+
+import SwiftUI
+
+@main
+struct greatnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ greatnessTests/greatnessTests.swift
@@ -0,0 +1,17 @@
+//
+//  greatnessTests.swift
+//  greatnessTests
+//
+//  Created by Сулейман Курбанов on 30.03.2026.
+//
+
+import Testing
+@testable import greatness
+
+struct greatnessTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
